50cf8b0686ae4812b798179bda63cfe742a74942
Yepeng Fan <yepeng.fan@hotmail.com>
2017-04-12 10:37:54 +1000

    Add eslintrc example

---
+++ eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+    },
+    "env": {
+      "es6": true
+    },
+    "parserOptions": {
+      "sourceType": "module"
+    },
+    "installedESLint": true,
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
